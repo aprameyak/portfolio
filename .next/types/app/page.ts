821dbf97bd60a782f219da42d9f75adf85838aa7
aprameyak <aprameyakannan@gmail.com>
2024-12-16 11:06:34 -0500

    commit

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/aprameyakannan/Documents/PortfolioWebSite/portfolio/app/page.tsx
+// File: /Users/aprameyakannan/Documents/portfolio/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
